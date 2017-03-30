dc5e622b26086dead81f86c878f706810ab899cd
ptruszcz <piotr.truszczynski95@gmail.com>
2017-03-31 00:42:07 +0200

    Really ugly steering and drawing

--- src/Asteroids/GameWindow.py
+++ src/Asteroids/GameWindow.py
@@ -1,33 +1,51 @@
 import pygame
+from Spaceship import Spaceship
+from Coordinates import Coordinates
 
+
+WINDOW_SIZE_X = 1024
+WINDOW_SIZE_Y = 768
+white = (255, 255, 255)
 
 class GameWindow:
     def __init__(self):
+        self._running = False
         self._screen = None
         self._clock = None
+        self._pressed_buttons = set()
+        self._spaceship = Spaceship(Coordinates(WINDOW_SIZE_X/2, WINDOW_SIZE_Y/2))
 
     def run(self):
         self._init()
 
-        while True:
+        while self._running:
+            self._clock.tick(15)
+
             for event in pygame.event.get():
                 self._handle_event(event)
-                self._render()
 
+            for key in self._pressed_buttons:
+                self._spaceship.steer(key)
+            self._spaceship.move()
+            self._render()
 
+        pygame.quit()
 
     def _init(self):
         pygame.init()
-        self._screen = pygame.display.set_mode((1024, 768), pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._screen = pygame.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y), pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._clock = pygame.time.Clock()
+        self._running = True
 
     def _handle_event(self, event):
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-        # do sth
+        if event.type == pygame.KEYDOWN:
+            self._pressed_buttons.add(event.key)
+        elif event.type == pygame.KEYUP:
+            self._pressed_buttons.remove(event.key)
+        elif event.type == pygame.QUIT:
+            self._running = False
 
     def _render(self):
         # render sth
-        # switch buffers as pygame uses double buffering
-        pygame.display.flip()
+        self._spaceship.draw(self._screen, white)
+        pygame.display.update()
--- src/Asteroids/Spaceship.py
+++ src/Asteroids/Spaceship.py
@@ -1,6 +1,56 @@
+import pygame
+import math
+from Vector import Vector
+from Coordinates import Coordinates
+
+
+# hardcoded and ugly - will be fixed soon :)
+# will be sprite later
 class Spaceship:
-    def __init__(self, coordinates, velocity=0, direction=0, acceleration=0):
-        self.coordinates = coordinates
-        self.velocity = velocity
-        self.direction = direction    # from 0 to 359
-        self.acceleration = acceleration
+    def __init__(self, coordinates, velocity=Vector(0, 0), direction=0, acceleration=0):
+        self._coordinates = coordinates
+        self._velocity = velocity
+        self._direction = direction    # from 0 to 359 where 0 is right and 90 is up
+        self._acceleration = acceleration
+
+    def draw(self, screen, color):
+        rect = pygame.Rect = (self._coordinates.x, self._coordinates.y, 20, 20)
+        pygame.draw.rect(screen, color, rect)
+
+    def steer(self, key):
+        if key == pygame.K_w:
+            self._accelerate(forward=True)
+        elif key == pygame.K_s:
+            self._accelerate(forward=False)
+        elif key == pygame.K_d:
+            self._rotate(clockwise=True)
+        elif key == pygame.K_a:
+            self._rotate(clockwise=False)
+        elif key == pygame.K_RETURN:
+            print("pew pew pew")
+
+    def move(self):
+        self._update_velocity()
+        self._coordinates.x += self._velocity.x_value
+        self._coordinates.y += self._velocity.y_value
+
+    def _accelerate(self, forward):
+        if forward:
+            self._acceleration += 0.01
+        else:
+            self._acceleration -= 0.01
+
+    def _rotate(self, clockwise):
+        if clockwise:
+            self._direction -= 1
+        else:
+            self._direction += 1
+        self._direction = self._direction % 360
+
+    def _update_velocity(self):
+        self._velocity.x_value += math.cos(math.radians(self._direction)) * self._acceleration
+        self._velocity.y_value += math.sin(math.radians(self._direction)) * self._acceleration
+
+        print("\nDirection: " + str(self._direction))
+        print("Acceleration: " + str(self._acceleration))
+        print("Velocity x: " + str(self._velocity.x_value) + "\nVelocity y: " + str(self._velocity.y_value))
