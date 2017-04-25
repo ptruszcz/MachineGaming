76f362c17823e4fc76c7affa72ca3e4df1067cca
Jakub <j.fajkowski@stud.elka.pw.edu.pl>
2017-04-25 23:53:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # bash script here
-sudo apt install cmake python3-dev libboost-all-dev libarmadillo-dev python-pygame
+sudo apt install cmake python3-dev python3-pip libboost-all-dev libarmadillo-dev
+sudo pip3 install pygame
 mkdir bin
 cd bin
 rm -rf *
