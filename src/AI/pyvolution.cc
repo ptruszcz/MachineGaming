300dedd155e9426bfb7cc3aeded65dbcdadeac4e
ptruszcz <piotr.truszczynski95@gmail.com>
2017-05-14 22:10:18 +0200

    Changed name of library to pyvolution

--- src/AI/pyvolution.cc
+++ src/AI/pyvolution.cc
@@ -1,4 +1,5 @@
 #include "NeuralNetwork.h"
+#include "EvolutionaryAlgorithm.h"
 #include <boost/python.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
@@ -37,7 +38,7 @@ public:
 };
 
 
-BOOST_PYTHON_MODULE(NeuralNetwork_Ext) {
+BOOST_PYTHON_MODULE(pyvolution) {
     class_<std::vector<double> >("vector_double")
             .def(vector_indexing_suite<std::vector<double> >());
 
@@ -46,4 +47,25 @@ BOOST_PYTHON_MODULE(NeuralNetwork_Ext) {
             .def("feed_forward_list", &NeuralNetworkProxy::feedForwardUsingPyList)
             .def("get_output", &NeuralNetworkProxy::getOutputAsVector)
             .add_property("fitness", &NeuralNetworkProxy::getFitness, &NeuralNetworkProxy::setFitness);
+
+    class_<std::vector<PNeuralNetwork> >("vector_nn")
+            .def(vector_indexing_suite<std::vector<PNeuralNetwork> >());
+
+    class_<EvolutionaryAlgorithmParameters>("EvolutionaryAlgorithmParameters", no_init)
+            .add_property("population_size", &EvolutionaryAlgorithmParameters::population_size)
+            .add_property("children_bred_per_generation",
+                          &EvolutionaryAlgorithmParameters::children_bred_per_generation)
+            .add_property("crossover_probability", &EvolutionaryAlgorithmParameters::crossover_probability)
+            .add_property("mutation_probability", &EvolutionaryAlgorithmParameters::mutation_probability)
+            .add_property("randomisation_probability", &EvolutionaryAlgorithmParameters::randomisation_probability)
+            .add_property("input_size", &EvolutionaryAlgorithmParameters::input_size)
+            .add_property("hidden_layers", &EvolutionaryAlgorithmParameters::hidden_layers)
+            .add_property("output_size", &EvolutionaryAlgorithmParameters::output_size);
+
+
+    class_<EvolutionaryAlgorithm>("EvolutionaryAlgorithm", init<EvolutionaryAlgorithmParameters>())
+            .def("breed", &EvolutionaryAlgorithm::breed)
+            .def("removeWeakestIndividuals", &EvolutionaryAlgorithm::removeWeakestIndividuals)
+        //.def("getCurrentGeneration", &EvolutionaryAlgorithm::getCurrentGeneration)
+            ;
 }
