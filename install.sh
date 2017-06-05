842d251a2dfa758a486ac2aba7e01d48f4ae52d6
Jakub <j.fajkowski@stud.elka.pw.edu.pl>
2017-06-06 01:52:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt update
 sudo apt install cmake python3-dev python3-pip python3-tk libboost-python-dev libboost-serialization-dev libboost-test-dev libarmadillo-dev
 sudo pip3 install pygame matplotlib
 mkdir bin
